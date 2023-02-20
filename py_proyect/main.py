872d22ac90f551c9f858549eecf4048492fde66f
Juan Manuel Bravo Martin <100429003@alumnos.uc3m.es>
2023-02-20 16:03:06 +0100

    Implementada la primera pagina web

--- py_proyect/main.py
+++ py_proyect/main.py
@@ -7,12 +7,13 @@ BASIC_READ = []
 BASIC_WRITE = []
 copia = ["IF","ID","EX","M","WB"]
 WB = True
-ACCESO_DATOS = 2
-ACCESO_ESCRITURA = 5
-ACCESO_LECTURA = 1
+ACCESO_DATOS = 2 ###### CICLOS DE ACCESO A LA MEMORIA DE DATOS ######
+ACCESO_ESCRITURA = 5 ###### CICLOS DE ACCESO A LA ESCRITURA DE DATOS ######
+ACCESO_LECTURA = 1 ###### CICLOS DE ACCESO A LA LECTURA DE DATOS ######
 
 
 def lastindex(lista,palabra):
+    """Devuelve la ultima aparicion de un objeto en una lista."""
     aparicion = 0
     for x in range(len(lista)):
         if lista[x] == palabra:
@@ -21,6 +22,7 @@ def lastindex(lista,palabra):
     return aparicion
 
 def firstindex(lista,palabra):
+    """Devuelve la primera aparicion de un objeto en una lista."""
     aparicion = 0
     for x in range(len(lista)):
         if lista[x] == palabra:
@@ -30,6 +32,7 @@ def firstindex(lista,palabra):
     
     return aparicion
 def set_basics():
+    """ Combina a informacion dada con los tipos de arrays, en favor de hacer la programacion mas facil"""
     if (ACCESO_DATOS !=1):
         for _ in range(ACCESO_DATOS -1):
             BASIC_WORK.insert(0,"IF")
@@ -46,6 +49,9 @@ def set_basics():
             BASIC_READ.insert(i,"M")
 
 def parser (programa):
+    """Funcion que divide el programa(un string) en diccionarios, asociandolos a una de las filas de la matriz, con su tipo
+    de instruccion. Una vez hecho esto, y de forma temporal, ejecuta las fucniones que detectan que las instrucciones se pisan
+    entre si"""
     contador = 0
     matriz = []
     partido = programa.split("\n")
@@ -69,11 +75,13 @@ def parser (programa):
             accion, var1, var2, var3 = arith_function(linea)
             matriz.append({"Accion": accion,"Continente": var1, "Ejecutor": [var2,var3], "Pipeline": variable})
         contador += 1
+    check_problems()
     check_consistency(matriz)
     printear(matriz)
 
 
 def check_consistency(matriz):
+    """Dada una matriz de filas del pipeline, busca si en la fila anterior se encuentra ocupado el hueco en el que tiene que entrar la nueva fila."""
     for x in range(1,len(matriz)):
         anterior = matriz[x-1]["Pipeline"].copy()
         siguiente = matriz[x]["Pipeline"].copy()
@@ -83,13 +91,16 @@ def check_consistency(matriz):
             indice_siguiente = firstindex(siguiente,i)
             dif = indice_anterior - indice_siguiente
             if dif >= 0:
-                print("PASO QUE VOY ARDIENDO")
                 for i in range(dif+1):
                     siguiente.insert(indice_siguiente,"-")
         matriz[x-1]["Pipeline"] = anterior.copy()
         matriz[x]["Pipeline"] = siguiente.copy()
     return matriz
+def check_problems():
+    """Funcion que comprueba los problemas en la matriz del tipo RaW o WaR"""
+    pass
 def memory_function(linea):
+    """Funcion que, dada una instruccion de acceso a memoria, devuelve los parametros en el orden estimado."""
     spliteado = linea.split(",")
     header = spliteado[0].split(" ")
     accion = header[0]
@@ -98,6 +109,7 @@ def memory_function(linea):
     return accion,var1,var2
 
 def arith_function(linea):
+    """Funcion que, dada una instruccion arithmetica, devuelve los parametros en el orden estimado."""
     spliteado = linea.split(",")
     header = spliteado[0].split(" ")
     accion = header[0]
@@ -107,10 +119,11 @@ def arith_function(linea):
     return accion,var1,var2,var3
 
 def printear(matriz):
+    """Funcion para mostrar la matriz."""
     constant = "                "
     total = len(matriz[-1]["Pipeline"])
     for x in range(total):
-        ciclo = "C" + str(x +1)
+        ciclo = "C" + str(x+1)
         constant = constant + ciclo + "\t"
         longitud = len(constant)
     constant += "\n"
@@ -126,7 +139,6 @@ def printear(matriz):
 
     print(constant)
     
-
-
-set_basics()
-parser(a)
+if __name__ == "__main__":
+    set_basics()
+    parser(a)
